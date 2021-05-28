ea05c5c1a827b81464a40a80a2e16f1cc8e9a3cc
wolenberg <wolembergjp@gmail.com>
2021-05-27 21:16:43 -0300

    categoria read implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,8 @@ import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './components/views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import {MatTableModule} from '@angular/material/table';
 
 
 
@@ -27,6 +30,7 @@ import {MatCardModule} from '@angular/material/card';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,7 +40,9 @@ import {MatCardModule} from '@angular/material/card';
     MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/views/categoria/categoria.service.ts
@@ -0,0 +1,24 @@
+import { environment } from './../../../../environments/environment';
+import { Categoria } from './categoria-read/categoria.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  baseUrl: String = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  findAll():Observable<Categoria[]> {
+    const url = this.baseUrl + "categorias"
+    
+    return this.http.get<Categoria[]>(url);
+
+  }
+
+}
